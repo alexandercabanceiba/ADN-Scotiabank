a4f5aa022cca880d792bd86516e5507e83a89e0b
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-08-18 13:29:11 -0500

    Se crea proyecto registry y se realizan cambio de nombre de microservicios

---
+++ registry-service/src/main/java/com/scotiabank/registry/RegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServiceApplication.class, args);
+	}
+
+}
