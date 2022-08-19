d1bb090504028c53c47f820c38427e321a603710
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-08-19 16:39:18 -0500

    .

--- ms-book/src/main/java/com/scotiabank/api/ScotiabankApplication.java
+++ ms-book/src/main/java/com/scotiabank/api/ScotiabankApplication.java
@@ -2,8 +2,10 @@ package com.scotiabank.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ScotiabankApplication {
 
 	public static void main(String[] args) {
