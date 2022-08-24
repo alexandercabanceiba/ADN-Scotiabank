315168ffc2c3df4599e4d3edff7c4cf977505f7d
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-08-24 12:17:33 -0500

    Se realiza el cambio de gateway a zuul

--- gateway-service/src/main/java/com/scotiabank/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/scotiabank/gateway/GatewayServiceApplication.java
@@ -3,8 +3,10 @@ package com.scotiabank.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayServiceApplication {
 
