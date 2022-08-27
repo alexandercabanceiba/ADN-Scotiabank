ebead797c7527f7dc9964194287eaefeb995f7d8
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-08-26 19:18:27 -0500

    feign

--- ms-library/src/main/java/com/scotiabank/library/infraestructura/controlador/ConsultaControladorBookRating.java
+++ ms-library/src/main/java/com/scotiabank/library/infraestructura/controlador/ConsultaControladorBookRating.java
@@ -4,6 +4,7 @@ import com.scotiabank.library.aplicacion.ManejadorConsultarBookRating;
 import com.scotiabank.library.dominio.book.Book;
 import com.scotiabank.library.dominio.library.Library;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ public class ConsultaControladorBookRating {
     private final ManejadorConsultarBookRating manejadorConsultarBookRating;
 
     @GetMapping("obtener-books-rating")
-    public Library obtenerTodosLosLibros() {
-        return manejadorConsultarBookRating.ejecutar();
+    public ResponseEntity<Library> obtenerTodosLosLibros() {
+        return ResponseEntity.ok(manejadorConsultarBookRating.ejecutar());
     }
 
 }
