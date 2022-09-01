23ec6d1058a24d03ebe49b7826f958506481cdea
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-08-31 23:30:30 -0500

    ćonfig histrix

--- ms-library/src/main/java/com/scotiabank/library/MsLibraryApplication.java
+++ ms-library/src/main/java/com/scotiabank/library/MsLibraryApplication.java
@@ -1,11 +1,16 @@
 package com.scotiabank.library;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletRegistration;
 
 @EnableEurekaClient
 @EnableFeignClients
@@ -16,6 +21,16 @@ public class MsLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsLibraryApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 
 }
