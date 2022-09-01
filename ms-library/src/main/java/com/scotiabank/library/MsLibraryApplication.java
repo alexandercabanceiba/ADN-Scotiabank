b6447196cef0778a248a7165155a5fc3bb43d8c8
Jhon Alexander Cabanillas Capote - Ceiba Software <alexander.cabanillas@ceiba.com.co>
2022-09-01 00:18:17 -0500

    cambios

--- ms-library/src/main/java/com/scotiabank/library/MsLibraryApplication.java
+++ ms-library/src/main/java/com/scotiabank/library/MsLibraryApplication.java
@@ -4,6 +4,7 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -12,9 +13,9 @@ import org.springframework.context.annotation.Bean;
 
 import javax.servlet.ServletRegistration;
 
-@EnableEurekaClient
-@EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
 public class MsLibraryApplication {
--- ms-library/src/main/java/com/scotiabank/library/infraestructura/controlador/ConsultaControladorBookRating.java
+++ ms-library/src/main/java/com/scotiabank/library/infraestructura/controlador/ConsultaControladorBookRating.java
@@ -3,6 +3,7 @@ package com.scotiabank.library.infraestructura.controlador;
 import com.scotiabank.library.aplicacion.consulta.ManejadorConsultarBookRating;
 import com.scotiabank.library.dominio.library.Library;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("/library")
 public class ConsultaControladorBookRating {
     private final ManejadorConsultarBookRating manejadorConsultarBookRating;
